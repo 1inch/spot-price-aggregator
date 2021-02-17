ff11809348f26211f5a02ca4806007341cde6e3d
Mikhail Melnik <by.zumzoom@gmail.com>
2021-02-17 11:31:59 +0300

    add usdt connector

---
+++ migrations/10_add_usdt_connector.js
@@ -0,0 +1,25 @@
+// const { tokens } = require('../test/helpers.js');
+
+// const OffchainOracle = artifacts.require('./OffchainOracle.sol');
+
+// const offchainOracleAddr = '0xe3c6B9C13739087c89Dcb83D3Ec15a767d829AA8';
+
+
+// module.exports = function (deployer, network) {
+//     return deployer.then(async () => {
+//         if (network === 'test' || network === 'coverage') {
+//             // migrations are not required for testing
+//             return;
+//         }
+
+//         const account = '0x11799622F4D98A24514011E8527B969f7488eF47';
+//         console.log('Deployer account: ' + account);
+//         console.log('Deployer balance: ' + (await web3.eth.getBalance(account)) / 1e18 + ' ETH');
+
+//         const offchainOracle = await OffchainOracle.at(offchainOracleAddr);
+//         await offchainOracle.addConnector(tokens.USDT);
+//     });
+// };
+
+module.exports = function (deployer, network) {
+}
