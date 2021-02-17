13941d9d4c132d5ba0389257df52390401ab9282
Mikhail Melnik <by.zumzoom@gmail.com>
2021-02-17 14:58:20 +0300

    fix test

--- test/OffchainOracle.js
+++ test/OffchainOracle.js
@@ -6,15 +6,16 @@ const uniswapV2Factory = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
 const initcodeHash = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
 const oneInchLP1 = '0xbAF9A5d4b0052359326A6CDAb54BABAa3a3A9643';
 
-const IdentityWrapper = artifacts.require('IdentityWrapper');
 const WethWrapper = artifacts.require('WethWrapper');
 const UniswapV2LikeOracle = artifacts.require('UniswapV2LikeOracle');
 const UniswapOracle = artifacts.require('UniswapOracle');
 const MooniswapOracle = artifacts.require('MooniswapOracle');
 const OffchainOracle = artifacts.require('OffchainOracle');
-const AaveWrapper = artifacts.require('AaveWrapper');
+const AaveWrapperV1 = artifacts.require('AaveWrapperV1');
+const AaveWrapperV2 = artifacts.require('AaveWrapperV2');
+const MultiWrapper = artifacts.require('MultiWrapper');
 
-const ADAI = '0x028171bCA77440897B824Ca71D1c56caC55b68A3';
+const ADAIV2 = '0x028171bCA77440897B824Ca71D1c56caC55b68A3';
 
 contract('OffchainOracle', function () {
     before(async function () {
@@ -22,21 +23,25 @@ contract('OffchainOracle', function () {
         this.uniswapOracle = await UniswapOracle.new();
         this.mooniswapOracle = await MooniswapOracle.new(oneInchLP1);
 
-        this.identityWrapper = await IdentityWrapper.new();
         this.wethWrapper = await WethWrapper.new();
-        this.aaveWrapper = await AaveWrapper.new();
-        await this.aaveWrapper.addMarkets([tokens.DAI]);
+        this.aaveWrapperV1 = await AaveWrapperV1.new();
+        this.aaveWrapperV2 = await AaveWrapperV2.new();
+        await this.aaveWrapperV1.addMarkets([tokens.DAI]);
+        await this.aaveWrapperV2.addMarkets([tokens.DAI]);
+        this.multiWrapper = await MultiWrapper.new(
+            [
+                this.wethWrapper.address,
+                this.aaveWrapperV1.address,
+                this.aaveWrapperV2.address,
+            ]
+        );
 
         this.offchainOracle = await OffchainOracle.new(
+            this.multiWrapper.address,
             [
                 this.uniswapV2LikeOracle.address,
                 this.uniswapOracle.address,
                 this.mooniswapOracle.address,
-            ],
-            [
-                this.identityWrapper.address,
-                this.wethWrapper.address,
-                this.aaveWrapper.address,
             ],
             [
                 tokens.NONE,
@@ -66,7 +71,7 @@ contract('OffchainOracle', function () {
     });
 
     it('dai -> adai', async function () {
-        const rate = await this.offchainOracle.getRate(tokens.DAI, ADAI);
+        const rate = await this.offchainOracle.getRate(tokens.DAI, ADAIV2);
         expect(rate).to.be.bignumber.equal(ether('1'));
     });
 });
