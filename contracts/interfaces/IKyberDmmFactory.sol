6574f083e6927dc4edebc2714613083dccb2f44c
Mikhail Melnik <by.zumzoom@gmail.com>
2021-09-17 21:02:21 +0800

    simplify interfaces

--- contracts/interfaces/IKyberDmmFactory.sol
+++ contracts/interfaces/IKyberDmmFactory.sol
@@ -6,23 +6,5 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
 interface IKyberDmmFactory {
-    function createPool(IERC20 tokenA, IERC20 tokenB, uint32 ampBps) external returns (address pool);
-
-    function setFeeConfiguration(address feeTo, uint16 governmentFeeBps) external;
-
-    function setFeeToSetter(address) external;
-
-    function getFeeConfiguration() external view returns (address feeTo, uint16 governmentFeeBps);
-
-    function feeToSetter() external view returns (address);
-
-    function allPools(uint256) external view returns (address pool);
-
-    function allPoolsLength() external view returns (uint256);
-
-    function getUnamplifiedPool(IERC20 token0, IERC20 token1) external view returns (address);
-
     function getPools(IERC20 token0, IERC20 token1) external view returns (address[] memory _tokenPools);
-
-    function isPool(IERC20 token0, IERC20 token1, address pool) external view returns (bool);
 }
