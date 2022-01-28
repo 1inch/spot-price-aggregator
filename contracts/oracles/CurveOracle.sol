7ab27fe60927fa4bab64190cf56f6d137de089e4
coolhill <osksva@student.chalmers.se>
2022-01-28 13:25:23 +0100

    underlying balances, check all registries one by one

--- contracts/oracles/CurveOracle.sol
+++ contracts/oracles/CurveOracle.sol
@@ -25,28 +25,54 @@ contract CurveOracle is IOracle {
 
         require(connector == _NONE, "connector should be NONE");
 
-        address registryAddress = addressProvider.get_address(0);
-        ICurveRegistry registry = ICurveRegistry(registryAddress);
-
+        ICurveRegistry registry;
         address pool;
+
+        // first check crypto registry (curve v2) for pools and select first one
+        registry = ICurveRegistry(addressProvider.get_address(5));
         pool = registry.find_pool_for_coins(address(_srcToken), address(_dstToken));
+
+        // if not found, check main registry and select first one
+        if (pool == address(0)){
+            registry = ICurveRegistry(addressProvider.get_address(0));
+            pool = registry.find_pool_for_coins(address(_srcToken), address(_dstToken));
+        }
+
+        // if not found, check factory registry (permissionless) and select first one
+        if (pool == address(0)){
+            registry = ICurveRegistry(addressProvider.get_address(3));
+            pool = registry.find_pool_for_coins(address(_srcToken), address(_dstToken));
+        }
+
         require(pool != address(0), "Pool does not exist");
 
         int128 a;
         int128 b;
         bool underlying;
+        uint256[8] memory balances;
         (a, b, underlying) = registry.get_coin_indices(pool, address(_srcToken), address(_dstToken));
 
-        if (underlying) {
+        if (!underlying) {
             rate = ICurveSwap(pool).get_dy(a, b, 1e6) * 1e12;
+            balances = registry.get_balances(pool);
         } else {
             rate = ICurveSwap(pool).get_dy_underlying(a, b, 1e6) * 1e12;
+            balances = registry.get_underlying_balances(pool);
         }
         require(rate != 0, "Swap rate not available");
-        
-        uint256 srcBalance = _srcToken.balanceOf(pool);
-        uint256 dstBalance = _dstToken.balanceOf(pool);
 
+        // this is for converting int128 -> uint
+        int128 j = 0;
+        uint256 srcBalance;
+        uint256 dstBalance;
+        for (uint256 i = 0; i < 8; i++) {
+            if (j == a) {
+                srcBalance = balances[i];
+            } else if (j == b) {
+                dstBalance = balances[i];
+            }
+            j++;
+        }
         weight = (srcBalance * dstBalance).sqrt();
     }
 }
