4c6fcce8939738ac29e475547123bc11506f2761
Mikhail Melnik <by.zumzoom@gmail.com>
2022-03-21 12:52:05 +0400

    use safecast

--- contracts/oracles/ChainlinkOracle.sol
+++ contracts/oracles/ChainlinkOracle.sol
@@ -4,10 +4,13 @@ pragma solidity 0.8.11;
 pragma abicoder v1;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import "../interfaces/IChainlink.sol";
 import "../interfaces/IOracle.sol";
 
 contract ChainlinkOracle is IOracle {
+    using SafeCast for int256;
+
     IChainlink public immutable chainlink;
     address private constant _QUOTE = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
     IERC20 private constant _ETH = IERC20(0x0000000000000000000000000000000000000000);
@@ -28,11 +31,10 @@ contract ChainlinkOracle is IOracle {
 
     function _getRate(IERC20 token) private view returns (uint256 rate, uint8 decimals) {
         (, int256 answer, , uint256 srcUpdatedAt, ) = chainlink.latestRoundData(token, _QUOTE);
-        require(answer >= 0, "CO: bad data from oracle");
         unchecked {
             require(block.timestamp < srcUpdatedAt + _RATE_TTL, "CO: rate too old");
         }
-        rate = uint256(answer);
+        rate = answer.toUint256();
         decimals = ERC20(address(token)).decimals();
         rate = rate * (10 ** (18 - decimals));
     }
