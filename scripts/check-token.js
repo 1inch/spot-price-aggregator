be782d28125e214962c3b532de7e99126a348cb8
Denis <zzomrot.ne@gmail.com>
2023-03-19 02:25:02 +0200

    Fix prices in check-tokens script

--- scripts/check-token.js
+++ scripts/check-token.js
@@ -1,15 +1,15 @@
 const { ethers } = require('hardhat');
 const { tokens } = require('../test/helpers');
 
-const usdPrice = (ethPrice) => { return ethers.utils.formatEther(BigInt(ethPrice) * BigInt(process.env.ETH_PRICE)); };
+const usdPrice = (ethPrice) => { return ethers.utils.formatEther(BigInt(Math.trunc(ethPrice)) * BigInt(process.env.SCRIPT_ETH_PRICE)); };
 
 async function main () {
-    if (!process.env.ETH_PRICE) {
-        throw new Error('Specify ETH_PRICE');
+    if (!process.env.SCRIPT_ETH_PRICE) {
+        throw new Error('Specify SCRIPT_ETH_PRICE');
     }
 
-    const networkName = process.env.NETWORK_NAME || 'mainnet';
-    const token = process.env.TOKEN || '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+    const networkName = process.env.SCRIPT_NETWORK_NAME || 'mainnet';
+    const token = process.env.SCRIPT_TOKEN || '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 
     const OffchainOracle = await ethers.getContractFactory('OffchainOracle');
     const offchainOracleInDeployments = require(`../deployments/${networkName}/OffchainOracle.json`);
@@ -27,21 +27,26 @@ async function main () {
     );
     await offchainOracle.deployed();
 
+    let decimals = 18;
+    try {
+        decimals = await (await ethers.getContractAt('IERC20Metadata', token)).decimals();
+    } catch {}
+
     console.log('======================');
-    console.log('Current state\'s price =', usdPrice(await deployedOffchainOracle.getRateToEth(token, true)));
+    console.log('Current state\'s price =', usdPrice(await deployedOffchainOracle.getRateToEth(token, true) / 10 ** (18 - decimals)));
 
     const oracles = await deployedOffchainOracle.oracles();
     for (let i = 0; i < oracles.allOracles.length; i++) {
         await offchainOracle.addOracle(oracles.allOracles[i], oracles.oracleTypes[i]);
 
-        const price = usdPrice(await offchainOracle.getRateToEth(token, true));
+        const price = usdPrice(await offchainOracle.getRateToEth(token, true) / 10 ** (18 - decimals));
         if (parseFloat(price) !== 0) {
             console.log(oracles.allOracles[i], 'oracle\'s price =', price);
 
             const oracle = await ethers.getContractAt('OracleBase', oracles.allOracles[i]);
             const pricesViaConnector = [];
             for (let j = 0; j < connectors.length; j++) {
-                let symbol;
+                let symbol = '---';
                 try {
                     if (tokens.NONE === connectors[j]) symbol = 'NONE';
                     else if (tokens.ETH === connectors[j]) symbol = 'ETH';
@@ -49,7 +54,7 @@ async function main () {
                     else symbol = await (await ethers.getContractAt('IERC20Metadata', connectors[j])).symbol();
 
                     const { rate, weight } = await oracle.getRate(token, weth, connectors[j]);
-                    pricesViaConnector.push({ connector: symbol, rate: parseFloat(usdPrice(rate)).toFixed(2), weight: weight.toString() });
+                    pricesViaConnector.push({ connector: symbol, rate: parseFloat(usdPrice(rate / 10 ** (18 - decimals))).toFixed(2), weight: weight.toString() });
                 } catch {}
             }
             console.table(pricesViaConnector.sort((a, b) => { return parseFloat(b.rate) - parseFloat(a.rate); }));
