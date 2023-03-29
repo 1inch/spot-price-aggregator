86068f40079a255b176f5471f8dc6e6772573ab1
Mikhail Melnik <by.zumzoom@gmail.com>
2023-03-29 13:18:49 +0400

    remove pragma abicoder v2

--- contracts/oracles/ChainlinkOracle.sol
+++ contracts/oracles/ChainlinkOracle.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
-pragma abicoder v2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
