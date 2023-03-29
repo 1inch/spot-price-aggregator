334adbf7c363a215fc2c7f49c413ee53dc364738
SevenSwen <tihonenko.v.v@gmail.com>
2023-03-29 15:02:40 +0700

    fix coverage

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,29 @@
 module.exports = {
     copyPackages: ['openzeppelin-solidity'],
     skipFiles: ['mocks'],
+    configureYulOptimizer: true,
+    solcOptimizerDetails: {
+        yul: true,
+        yulDetails: {
+            optimizerSteps:
+                "dhfoDgvlfnTUtnIf" +               // None of these can make stack problems worse
+                "[" +
+                    "xa[r]EscLM" +                 // Turn into SSA and simplify
+                    "cCTUtTOntnfDIl" +             // Perform structural simplification
+                    "Lcl" +                        // Simplify again
+                    "Vcl [j]" +                    // Reverse SSA
+
+                    // should have good "compilability" property here.
+
+                    "Tpel" +                       // Run functional expression inliner
+                    "xa[rl]" +                     // Prune a bit more in SSA
+                    "xa[r]cL" +                    // Turn into SSA again and simplify
+                    "gvf" +                        // Run full inliner
+                    "CTUca[r]LSsTFOtfDnca[r]Ilc" + // SSA plus simplify
+                "]" +
+                "jml[jl] VcTOcl jml",
+        },
+    },
     mocha: {
         grep: "_GasCheck",
         invert: true,
