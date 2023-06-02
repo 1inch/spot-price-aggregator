24b8dc61bd6169512a9d59d5da4f39ec57ee7c78
Edozie I <112929878+zodenode@users.noreply.github.com>
2023-06-02 15:17:28 +0400

    Update MultiWrapper.sol
    Added Natspecs for MultiWrapper.sol

--- contracts/MultiWrapper.sol
+++ contracts/MultiWrapper.sol
@@ -7,6 +7,12 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./interfaces/IWrapper.sol";
 
+/**
+ * @title MultiWrapper
+ * @notice The MultiWrapper contract allows for the management of multiple wrappers that can be used to wrap tokens.
+ * Wrappers are contracts that enable the conversion of tokens from one protocol to another.
+ * The contract provides functions to add and remove wrappers, as well as get information about the wrapped tokens and their conversion rates.
+ */
 contract MultiWrapper is Ownable {
     using SafeMath for uint256;
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -19,15 +25,25 @@ contract MultiWrapper is Ownable {
 
     EnumerableSet.AddressSet private _wrappers;
 
+    /**
+     * @dev Initializes the MultiWrapper contract.
+     * @param existingWrappers An array of existing wrapper contracts to be added during initialization.
+     * @notice The constructor adds the existing wrappers to the contract and emits events for each added wrapper.
+     * If a wrapper is already added, it will throw a WrapperAlreadyAdded error and revert the transaction.
+     */
     constructor(IWrapper[] memory existingWrappers) {
         unchecked {
             for (uint256 i = 0; i < existingWrappers.length; i++) {
-                if(!_wrappers.add(address(existingWrappers[i]))) revert WrapperAlreadyAdded();
+                if (!_wrappers.add(address(existingWrappers[i]))) revert WrapperAlreadyAdded();
                 emit WrapperAdded(existingWrappers[i]);
             }
         }
     }
 
+    /**
+     * @dev Returns an array of all the wrappers currently added to the contract.
+     * @return allWrappers An array of IWrapper contracts representing all the wrappers.
+     */
     function wrappers() external view returns (IWrapper[] memory allWrappers) {
         allWrappers = new IWrapper[](_wrappers.length());
         unchecked {
@@ -37,16 +53,37 @@ contract MultiWrapper is Ownable {
         }
     }
 
+    /**
+     * @dev Adds a new wrapper contract to the MultiWrapper.
+     * @param wrapper The address of the wrapper contract to be added.
+     * @notice Only the contract owner can add a new wrapper.
+     * If the wrapper is already added, it will throw a WrapperAlreadyAdded error and revert the transaction.
+     */
     function addWrapper(IWrapper wrapper) external onlyOwner {
-        if(!_wrappers.add(address(wrapper))) revert WrapperAlreadyAdded();
+        if (!_wrappers.add(address(wrapper))) revert WrapperAlreadyAdded();
         emit WrapperAdded(wrapper);
     }
 
+    /**
+     * @dev Removes a wrapper contract from the MultiWrapper.
+     * @param wrapper The address of the wrapper contract to be removed.
+     * @notice Only the contract owner can remove a wrapper.
+     * If the wrapper is not found, it will throw an UnknownWrapper error and revert the transaction.
+     */
     function removeWrapper(IWrapper wrapper) external onlyOwner {
-        if(!_wrappers.remove(address(wrapper))) revert UnknownWrapper();
+        if (!_wrappers.remove(address(wrapper))) revert UnknownWrapper();
         emit WrapperRemoved(wrapper);
     }
 
+    /**
+     * @dev Retrieves the wrapped tokens and their conversion rates for a given token.
+     * @param token The ERC20 token for which to retrieve the wrapped tokens and conversion rates.
+     * @return wrappedTokens An array of wrapped tokens that can be obtained by wrapping the input token.
+     * @return rates An array of conversion rates corresponding to the wrapped tokens.
+     * @notice This function iterates over the wrappers to determine the wrapped tokens and their conversion rates.
+     * It returns an array of wrapped tokens and an array of rates, including the input token and a rate of 1e18 for it.
+     * If a wrapper fails during the iteration, it will be skipped and the process will continue.
+     */
     function getWrappedTokens(IERC20 token) external view returns (IERC20[] memory wrappedTokens, uint256[] memory rates) {
         unchecked {
             IERC20[] memory memWrappedTokens = new IERC20[](20);
@@ -72,10 +109,14 @@ contract MultiWrapper is Ownable {
                                     memRates[len] = rate.mul(rate2).div(1e18);
                                     len += 1;
                                 }
-                            } catch { continue; }
+                            } catch {
+                                continue;
+                            }
                         }
                     }
-                } catch { continue; }
+                } catch {
+                    continue;
+                }
             }
             wrappedTokens = new IERC20[](len + 1);
             rates = new uint256[](len + 1);
