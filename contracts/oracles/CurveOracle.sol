ad79222e13ae9211c77e125197c908864902414b
Denis <zzomrot.ne@gmail.com>
2023-08-10 15:33:14 +0300

    Remove redundant imports

--- contracts/oracles/CurveOracle.sol
+++ contracts/oracles/CurveOracle.sol
@@ -3,7 +3,6 @@
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "../interfaces/IOracle.sol";
 import "../interfaces/ICurveProvider.sol";
