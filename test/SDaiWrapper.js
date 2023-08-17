a688c6fa0ed4c556e7866024f6ebaaee156a996b
Denis <zzomrot.ne@gmail.com>
2023-08-17 20:28:05 +0300

    Add sDaiWrapper

---
+++ test/SDaiWrapper.js
@@ -0,0 +1,26 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { ethers } = require('hardhat');
+const { expect, ether, deployContract } = require('@1inch/solidity-utils');
+const { tokens } = require('./helpers.js');
+
+describe('SDaiWrapper', function () {
+    async function initContracts () {
+        const sDaiWrapper = await deployContract('SDaiWrapper', [tokens.sDAI, tokens.DAI]);
+        const sDai = await ethers.getContractAt('ISDai', tokens.sDAI);
+        return { sDaiWrapper, sDai };
+    }
+
+    it('DAI -> sDAI', async function () {
+        const { sDaiWrapper, sDai } = await loadFixture(initContracts);
+        const response = await sDaiWrapper.wrap(tokens.DAI);
+        expect(response.rate).to.equal(await sDai.previewDeposit(ether('1')));
+        expect(response.wrappedToken).to.equal(tokens.sDAI);
+    });
+
+    it('sDAI -> DAI', async function () {
+        const { sDaiWrapper, sDai } = await loadFixture(initContracts);
+        const response = await sDaiWrapper.wrap(tokens.sDAI);
+        expect(response.rate).to.equal(await sDai.previewRedeem(ether('1')));
+        expect(response.wrappedToken).to.equal(tokens.DAI);
+    });
+});
