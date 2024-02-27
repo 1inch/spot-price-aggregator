20025dda3aff05234475c3e89575cdcc52e7a02a
Denis <zzomrot.ne@gmail.com>
2024-02-27 16:46:02 +0000

    Increase diff in tests for deprecated Mooniswap

--- test/oracles/MooniswapOracle.js
+++ test/oracles/MooniswapOracle.js
@@ -15,26 +15,26 @@ describe('MooniswapOracle', function () {
 
     it('ETH -> DAI', async function () {
         const { mooniswapOracle, uniswapV3Oracle } = await loadFixture(initContracts);
-        await testRate([tokens.ETH, tokens.WETH], tokens.DAI, tokens.NONE, mooniswapOracle, uniswapV3Oracle);
+        await testRate([tokens.ETH, tokens.WETH], tokens.DAI, tokens.NONE, mooniswapOracle, uniswapV3Oracle, 0.2);
     });
 
     it('DAI -> ETH', async function () {
         const { mooniswapOracle, uniswapV3Oracle } = await loadFixture(initContracts);
-        await testRate(tokens.DAI, [tokens.ETH, tokens.WETH], tokens.NONE, mooniswapOracle, uniswapV3Oracle);
+        await testRate(tokens.DAI, [tokens.ETH, tokens.WETH], tokens.NONE, mooniswapOracle, uniswapV3Oracle, 0.2);
     });
 
     it('ETH -> USDC', async function () {
         const { mooniswapOracle, uniswapV3Oracle } = await loadFixture(initContracts);
-        await testRate([tokens.ETH, tokens.WETH], tokens.USDC, tokens.NONE, mooniswapOracle, uniswapV3Oracle);
+        await testRate([tokens.ETH, tokens.WETH], tokens.USDC, tokens.NONE, mooniswapOracle, uniswapV3Oracle, 0.2);
     });
 
     it('USDC -> 1INCH', async function () {
         const { mooniswapOracle, uniswapV3Oracle } = await loadFixture(initContracts);
-        await testRate(tokens.USDC, tokens['1INCH'], tokens.NONE, mooniswapOracle, uniswapV3Oracle, 0.06);
+        await testRate(tokens.USDC, tokens['1INCH'], tokens.NONE, mooniswapOracle, uniswapV3Oracle, 0.2);
     });
 
     it('ETH -> USDC -> 1INCH', async function () {
         const { mooniswapOracle, uniswapV3Oracle } = await loadFixture(initContracts);
-        await testRate([tokens.ETH, tokens.WETH], tokens['1INCH'], tokens.USDC, mooniswapOracle, uniswapV3Oracle, 0.075);
+        await testRate([tokens.ETH, tokens.WETH], tokens['1INCH'], tokens.USDC, mooniswapOracle, uniswapV3Oracle, 0.2);
     });
 });
